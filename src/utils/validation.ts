01325cd409bc6fb56abe1ceaa0cbcca8f156f41a
Guru Dhupkar <guruprasaddhupkar@gmail.com>
2025-08-14 11:35:07 +0530

    Login zod schema

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -24,3 +24,22 @@ export const registerschema = z.object({
             "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
         ),
 })
+
+export const loginschema = z.object({
+
+    email: z
+        .string()
+        .trim()
+        .min(5, "Email too short")
+        .max(50, "Email too long")
+        .email("Invalid email format"),
+
+    passwordhash: z
+        .string()
+        .min(5, "Password must be at least 5 characters")
+        .max(20, "Password too long")
+        .regex(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]+$/,
+            "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+        ),
+})
