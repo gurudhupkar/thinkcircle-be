8d30636f563ca862f25f6f41ae2ff6ccfb6b91d0
Guru Dhupkar <guruprasaddhupkar@gmail.com>
2025-08-15 01:36:13 +0530

    Middleware added

---
+++ src/middleware/authmiddleware.ts
@@ -0,0 +1,53 @@
+import jwt from "jsonwebtoken"
+import * as dotenv from "dotenv"
+import { Request, Response, NextFunction } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const JWT_USER_SEC =  process.env.SECRET_KEY || ""
+const prisma = new PrismaClient();
+if(!JWT_USER_SEC){
+    console.log("JWT secrect missing");
+}
+export interface AuthRequest extends Request {
+  user?: {
+    id: string;
+    name: string;
+    email: string;
+  };
+}
+
+export async function userMiddleware(req: AuthRequest, res: Response, next: NextFunction) {
+  try {
+//   const header = req.headers['authorization'];
+
+//   if (!header) {
+//     return res.status(401).json({ message: "Authorization header missing" });
+//   }
+//       const decoded = jwt.verify(header, JWT_USER_SEC as string) as { id: string };
+//     console.log(decoded)
+ const authHeader = req.headers["authorization"];
+    if(!authHeader){
+        return res.status(400).json({
+            message:"Authorization header missing"
+        })
+    }
+
+    const token = authHeader;
+    const decoded = jwt.verify(token, JWT_USER_SEC) as { id: string };
+
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.id },
+      select: { id: true, name: true, email: true }
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found or deleted" });
+    }
+
+    req.user = user; 
+    next();
+  } catch (err) {
+    console.log(err)
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+}
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,54 +1,59 @@
 import express from "express"
 import bcyrpt from "bcrypt"
 import { Router } from "express"
-import { registerschema } from "../utils/validation";
+import { loginschema, registerschema } from "../utils/validation";
 import { PrismaClient } from "@prisma/client"
 import { error } from "console";
 import { success } from "zod";
-const userRouter :Router = Router();
+import jwt from "jsonwebtoken"
+import { AuthRequest, userMiddleware } from "../middleware/authmiddleware";
+const userRouter: Router = Router();
+const JWT_USER_SEC = process.env.SECRET_KEY || ""
 const prisma = new PrismaClient();
-const SALT_ROUNDS= 10
-userRouter.post("/register" , async(req,res)=>{
-   const parseddatawithsuccess = registerschema.safeParse(req.body)
-    if(!parseddatawithsuccess.success){
-       return res.status(400).json({
-            message:"Please enter all valid fields",
-            success:false
+if (!JWT_USER_SEC) {
+    console.log("JWT secrect missing");
+}
+const SALT_ROUNDS = 10
+userRouter.post("/register", async (req, res) => {
+    const parseddatawithsuccess = registerschema.safeParse(req.body)
+    if (!parseddatawithsuccess.success) {
+        return res.status(400).json({
+            message: "Please enter all valid fields",
+            success: false
         })
     }
-    const {name ,email , passwordHash} = parseddatawithsuccess.data
-    try{
-        const hash = await bcyrpt.hash(passwordHash , SALT_ROUNDS)
-     const user = await prisma.user.create({
-            data:{
+    const { name, email, passwordHash } = parseddatawithsuccess.data
+    try {
+        const hash = await bcyrpt.hash(passwordHash, SALT_ROUNDS)
+        const user = await prisma.user.create({
+            data: {
                 name,
                 email,
-                passwordHash:hash
+                passwordHash: hash
             }
         })
-        if(user){
+        if (user) {
             res.status(200).json({
-                message:"user created successfully",
-                success:true,
-                name : user.name,
-                email:user.email
+                message: "user created successfully",
+                success: true,
+                name: user.name,
+                email: user.email
             })
         }
-        else{
-           res.status(400).json({
-            message:"User not created ",
-            success:false
-           })
+        else {
+            res.status(400).json({
+                message: "User not created ",
+                success: false
+            })
         }
     }
-    catch(error :any){
+    catch (error: any) {
         console.log(error)
         res.status(500).json({
-            message:"Email alredy exists",
-            success:false
+            message: "Email alredy exists",
+            success: false
         })
     }
 
 })
-
-export {userRouter}
+export { userRouter }
