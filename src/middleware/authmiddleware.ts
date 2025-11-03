b8716f6ef866171c531dfa67b927ed7fb8c4ef3e
Guru Dhupkar <guruprasaddhupkar@gmail.com>
2025-11-03 14:58:28 +0530

    .env prod error

--- src/middleware/authmiddleware.ts
+++ src/middleware/authmiddleware.ts
@@ -1,36 +1,42 @@
-import jwt from "jsonwebtoken"
-import * as dotenv from "dotenv"
+import jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
 import { Request, Response, NextFunction } from "express";
 import { PrismaClient } from "@prisma/client";
+import path = require("path");
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
-const JWT_USER_SEC =  process.env.SECRET_KEY || ""
+const JWT_USER_SEC = process.env.SECRET_KEY || "";
 const prisma = new PrismaClient();
-if(!JWT_USER_SEC){
-    console.log("JWT secrect missing");
+if (!JWT_USER_SEC) {
+  console.log("JWT secrect missing");
 }
 export interface AuthRequest extends Request {
   user?: {
     id: string;
     firstname: string;
-    lastname:string;
+    lastname: string;
     email: string;
   };
 }
 
-export async function userMiddleware(req: AuthRequest, res: Response, next: NextFunction) {
+export async function userMiddleware(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) {
   try {
-//   const header = req.headers['authorization'];
+    //   const header = req.headers['authorization'];
 
-//   if (!header) {
-//     return res.status(401).json({ message: "Authorization header missing" });
-//   }
-//       const decoded = jwt.verify(header, JWT_USER_SEC as string) as { id: string };
-//     console.log(decoded)
- const authHeader = req.headers["authorization"];
-    if(!authHeader){
-        return res.status(400).json({
-            message:"Authorization header missing"
-        })
+    //   if (!header) {
+    //     return res.status(401).json({ message: "Authorization header missing" });
+    //   }
+    //       const decoded = jwt.verify(header, JWT_USER_SEC as string) as { id: string };
+    //     console.log(decoded)
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) {
+      return res.status(400).json({
+        message: "Authorization header missing",
+      });
     }
 
     const token = authHeader;
@@ -38,17 +44,17 @@ export async function userMiddleware(req: AuthRequest, res: Response, next: Next
 
     const user = await prisma.user.findUnique({
       where: { id: decoded.id },
-      select: { id: true, firstname: true, lastname:true, email: true }
+      select: { id: true, firstname: true, lastname: true, email: true },
     });
 
     if (!user) {
       return res.status(401).json({ message: "User not found or deleted" });
     }
 
-    req.user = user; 
+    req.user = user;
     next();
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return res.status(401).json({ message: "Invalid or expired token" });
   }
 }
